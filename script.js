93b75fbc2c7dd103da8d8e819009b94de5153df2
Nina <mysha76@mail.ru>
2020-09-09 13:16:15 +0300

    Добавлена разметка дороги. Добавлены соперники

--- script.js
+++ script.js
@@ -15,20 +15,71 @@ const keys = {
 const setting = {
   start: false,
   score: 0,
-  speed: 3
+  speed: 3,
+  traffic: 3
+};
+
+const getQuantityElements = (heightElement) => {
+  return document.documentElement.clientHeight / heightElement + 1;
 };
 
 const startGame = () => {
   start.classList.add('hide');
+
+  for (let i = 0; i < getQuantityElements(100); i++) {
+    const line = document.createElement('div');
+    line.classList.add('line');
+    line.style.top = (i * 100) + 'px';
+    line.y = i * 100;
+    gameArea.appendChild(line);
+  }
+
+  for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++) {
+    const enemy = document.createElement('div');
+    enemy.classList.add('enemy');
+    enemy.y = -150 * setting.traffic * (i + 1);
+    enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+    enemy.style.top = enemy.y + 'px';
+    enemy.style.background = 'transparent url(./image/player1.png) center / cover no-repeat';
+    gameArea.appendChild(enemy);
+  }
+
   setting.start = true;
   gameArea.appendChild(car);
+  setting.x = car.offsetLeft;
+  setting.y = car.offsetTop;
+
   requestAnimationFrame(playGame);
 };
 
 const playGame = () => {
-  console.log('Play game!');
 
   if (setting.start) {
+
+    moveRoad();
+
+    moveEnemy();
+
+    if (keys.ArrowLeft && setting.x > 0) {
+      setting.x -= setting.speed;
+    }
+
+    if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) {
+      setting.x += setting.speed;
+    }
+
+    if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)) {
+      setting.y += setting.speed;
+    }
+
+    if (keys.ArrowUp && setting.y > 0) {
+      setting.y -= setting.speed;
+    }
+
+    car.style.left = setting.x + 'px';
+    car.style.top = setting.y + 'px';
+
+
     requestAnimationFrame(playGame);
   }
 };
@@ -41,6 +92,34 @@ const startRun = (event) => {
 const stopRun = (event) => {
   event.preventDefault();
   keys[event.key] = false;
+};
+
+const moveRoad = () => {
+  let lines = document.querySelectorAll('.line');
+
+  lines.forEach((line) => {
+    line.y += setting.speed;
+    line.style.top = line.y + 'px';
+
+    if (line.y >= document.documentElement.clientHeight) {
+      line.y = -100;
+    }
+  });
+};
+
+const moveEnemy = () => {
+  let enemies = document.querySelectorAll('.enemy');
+
+  enemies.forEach((enemy) => {
+    enemy.y += setting.speed / 2;
+    enemy.style.top = enemy.y + 'px';
+
+    if (enemy.y >= document.documentElement.clientHeight) {
+      enemy.y = -150 * setting.traffic;
+      enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+    }
+  });
+
 };
 
 start.addEventListener('click', startGame);
